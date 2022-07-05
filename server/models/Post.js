b093531f9c4da999748f057afb257ff3cd5424d5
hyeb96 <eban.hdit@gmail.com>
2022-07-05 18:31:07 -0400

    adding dabatbase

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const PostSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-      },
-      description: {
-        type: String,
-        required: true,
-      }
-})
-module.exports = mongoose.model('Post', PostSchema);
+// const mongoose = require('mongoose');
+// const PostSchema = mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true,
+//       },
+//       description: {
+//         type: String,
+//         required: true,
+//       }
+// })
+// module.exports = mongoose.model('Post', PostSchema);
