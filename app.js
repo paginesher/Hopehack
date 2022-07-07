c6e126a7c581c47710e06b467328b40e720680e5
paginesher <paginesher@gmail.com>
2022-07-06 21:33:04 -0400

    added path resolve

--- app.js
+++ app.js
@@ -9,34 +9,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //connect to database 
-mongoose.connect('mongodb+srv://hyeb96:Sm17emina@cluster0.yyfegbj.mongodb.net/hopehack', { useNewUrlParser: true }, { useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://hyeb96:Sm17emina@cluster0.yyfegbj.mongodb.net/hopehacks', { useNewUrlParser: true }, { useUnifiedTopology: true })
 
 //create schema 
-const contactSchema = {
+const contactsSchema = {
     name: String,
     email: String,
     message: String
 }
 
 //create model
-const contact = mongoose.model("contact", contactSchema)
+const contacts = mongoose.model("contacts", contactsSchema)
 
 app.post("/contact", (req, res) => {
-    let newContact = new contact({
+    let newContacts = new contacts({
         name: req.body.name,
         email: req.body.email,
         message: req.body.message
     });
-    newContact.save();
+    newContacts.save();
     res.redirect('/contact');
 })
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public.pics"));
 
+app.use("/public", express.static(path.resolve(__dirname, "public")))
+app.use("/pics", express.static(path.resolve(__dirname, "public/pics")))
 
 //route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/home.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.get('/aboutus', (req, res) => {
